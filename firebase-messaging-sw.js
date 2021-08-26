786f8bda20489ac017614f69131876c369c1a839
Gdocal <Gdocal@gmail.com>
2021-08-26 20:33:48 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAYmw2WFzZRmiDWoglR-yohkDu873mDOXc",
-    authDomain: "test-notitication.firebaseapp.com",
-    projectId: "test-notitication",
-    storageBucket: "test-notitication.appspot.com",
-    messagingSenderId: "532209433002",
-    appId: "1:532209433002:web:c56a6d093e04a6cc4313d3",
-    measurementId: "G-STZ558KMRW"
+   apiKey: "AIzaSyAYmw2WFzZRmiDWoglR-yohkDu873mDOXc",
+  authDomain: "test-notitication.firebaseapp.com",
+  projectId: "test-notitication",
+  storageBucket: "test-notitication.appspot.com",
+  messagingSenderId: "532209433002",
+  appId: "1:532209433002:web:f1d1f7678d26954b4313d3",
+  measurementId: "G-0WSN29YLRH"
 });
 
 const messaging = firebase.messaging();
